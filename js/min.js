365fc64819624d57109e8d9ed2535b52ec328662
TasneemDev02 <149237468+MD-youcef@users.noreply.github.com>
2023-11-12 16:05:45 +0100

    fullName
    document.querySelector

--- js/min.js
+++ js/min.js
@@ -1,2 +1,13 @@
-//rècupérer les valeurs des champs du formulaire
-const fullname = document.querySelector('.fullname');
+//Récupérer les valeurs des champs du formulaire
+const fullName = document.querySelector('.fullName');
+const dateofBirth = document.querySelector('.dateofBirth');
+const gender = document.querySelector('.select[Name="gender"]');
+const task1 = document.querySelector('.task1');
+const task2 = document.querySelector('.task2');
+const task3 = document.querySelector('.task3');
+
+
+//Récupérer les bouttons
+const result = document.querySelector('.result');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
